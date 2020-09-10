11f9c1946a64ff0dc52d8bdf58034a6275eca850
Kim Dev <kiki.im.dev@gmail.com>
2020-09-10 22:28:44 +0800

    Changed index.html

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  "pwa": {
+    "name": 'AMPJ Telkom | Aplikasi Monitoring Perangkat Jaringan Telkom',
+    "themeColor": '#ffffff',
+    "msTileColor": '#ffffff',
+    "appleMobileWebAppCapable": 'yes',
+    "appleMobileWebAppStatusBarStyle": 'white',
+  }
 }
